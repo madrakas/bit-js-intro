d3d5eb894e10be217f082031eec055f7faceb8ae
Arvydas <arvydas.simbelis@gmail.com>
2023-09-19 11:16:20 +0300

    Kintamieji

---
+++ number.js
@@ -0,0 +1,22 @@
+/* Java kintamieji
+
+- number (teigiami/neigiami, sveikieji/desimtainiai, normalus/nenormalus)
+- string 
+- boolean
+- array 
+- object */
+
+const amzius=99;
+let pinigine=0;
+var minusas = -7.56;
+
+const x1 = 1;
+const x2 = 2;
+const x3 = 3;
+const x4 = 4;
+const x5 = 5;
+
+
+console.log(x5);
+console.log(pinigine);
+console.log(amzius);
