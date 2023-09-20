8723976be42ea55ffb5028617c0495f41ac0774d
Arvydas <arvydas.simbelis@gmail.com>
2023-09-20 17:25:05 +0300

    .gitignore format; numbers.js optimizations

--- kintamieji/number.js
+++ kintamieji/number.js
@@ -15,20 +15,24 @@ const x2 = 2;
 const x3 = 3;
 const x4 = 4;
 const x5 = 5;
+const infinity1=1/0;
 
 console.log(x5);
 console.log(pinigine);
 console.log(amzius);
 
-const n1 = 7;
-const n2 = 5;
+const sum2 =x1+x2;
+const diff2=x1-x5;
+const mltp= x5*x3;
+const div2= x4/x3;
+const root= 9 **-2;
 
-const sum = n1 + n2;
-const diff = n1-n2;
-const div=n1-n2;
-const complex=(2+2)*2;
-
-console.log("Suma: ", sum);
-console.log(diff);
-console.log(complex);
-console.log("aaa");
+console.log("x1+x2=",sum2);
+console.log("x1-x5=",diff2);
+console.log("x5*x3=",mltp);
+console.log("x4/x3=",div2);
+console.log("9 **-2=",root);
+console.log("Infinity1",infinity1);
+console.log("Div by inf", 1000/infinity1);
+console.log("Inf times Inf", infinity1*infinity1);
+console.log("1/3=", 1/3);
