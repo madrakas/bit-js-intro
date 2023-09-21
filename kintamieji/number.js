9de8766fafc45956536cf59180eaed5e96a390b3
Arvydas <arvydas.simbelis@gmail.com>
2023-09-21 08:52:04 +0300

    Infinity & rem

--- kintamieji/number.js
+++ kintamieji/number.js
@@ -15,24 +15,30 @@ const x2 = 2;
 const x3 = 3;
 const x4 = 4;
 const x5 = 5;
-const infinity1=1/0;
+const infinity1 = 1 / 0;
 
 console.log(x5);
 console.log(pinigine);
 console.log(amzius);
 
-const sum2 =x1+x2;
-const diff2=x1-x5;
-const mltp= x5*x3;
-const div2= x4/x3;
+const sum2 = x1 + x2;
+const diff2 = x1 - x5;
+const mltp= x5 * x3;
+const div2= x4 / x3;
 const root= 9 ** 0.5;
 
-console.log("x1+x2=",sum2);
-console.log("x1-x5=",diff2);
-console.log("x5*x3=",mltp);
-console.log("x4/x3=",div2);
-console.log("9 ** 0.5=",root);
-console.log("Infinity1",infinity1);
-console.log("Div by inf", 1000/infinity1);
-console.log("Inf times Inf", infinity1*infinity1);
-console.log("1/3=", 1/3);
+console.log( "x1+x2=" , sum2);
+console.log( "x1-x5=" , diff2);
+console.log( "x5*x3=" , mltp);
+console.log( "x4/x3=" , div2);
+console.log( "9 ** 0.5=" , root);
+console.log( "Infinity1", infinity1);
+console.log( "Div by inf" , 1000/infinity1);
+console.log( "Inf times Inf" , infinity1*infinity1);
+console.log( "1/3=" , 1/3);
+
+const begalybe= Infinity;
+const minusBegalybe= -Infinity;
+
+const liekana = 7 % 5;
+console.log( "liekana=" , liekana);
