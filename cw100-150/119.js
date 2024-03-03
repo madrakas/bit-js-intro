6d87a7f5a927736613adf7a98dfa0d3f40e25468
Arvydas Šimbelis (https://github.com/madrakas) <arvydas.simbelis@gmail.com>
2024-03-03 22:36:47 +0200

    cw

--- cw100-150/119.js
+++ cw100-150/119.js
@@ -1,17 +1,37 @@
 console.clear();
 // https://www.codewars.com/kata/54129112fb7c188740000162/train/javascript
 function prefill(n, v) {
+    console.log(n,v);
+   
+    // console.log('zzzz', parseInt(Infinity));
+
+
+
+    if (parseInt(n) != NaN){
+        const x = parseInt(n);
+        // console.log('aaa'); 
+    } else {
+        throw new TypeError (n + ' is invalid');
+    }
+
     if (n > 0){
-        return result = Array(n).fill(v);
-    }else if (Number.isInteger(n)){
-        // throw TypeError("n must be integer")
-        return (n + ' is invalid');
+        return result = Array(x).fill(v);
+    
+    }else if(n === 0){
+        return [];
+    }else if (n === Infinity || n === -Infinity || n === NaN){
+        throw new Error (n + ' is invalid');
+    }else if (!Number.isInteger(n)){
+        throw new TypeError (n + ' is invalid');
     }else{
         return [];
     }
-}
+ }
 
 // console.log(prefill(3,1), [1,1,1]);
+console.log(prefill(Infinity, undefined), [1,1,1]);
+
+
 // console.log(prefill(2,'abc'), ['abc','abc']);
 // console.log(prefill('1',1), [1]);
-console.log(prefill(3, prefill(2,'2d')), [['2d','2d'],['2d','2d'],['2d','2d']]);
+// console.log(prefill(3, prefill(2,'2d')), [['2d','2d'],['2d','2d'],['2d','2d']]);
