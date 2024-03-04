2b08a2014441ba870b9279d012a800edd7c77368
Arvydas <arvydas.simbelis@gmail.com>
2024-03-04 21:49:12 +0200

    CW

--- cw100-150/119.js
+++ cw100-150/119.js
@@ -1,35 +1,43 @@
 console.clear();
+
+
 // https://www.codewars.com/kata/54129112fb7c188740000162/train/javascript
 function prefill(n, v) {
-    console.log(n,v);
-   
-    // console.log('zzzz', parseInt(Infinity));
+    if (n == Infinity || n == -Infinity){
+        throw new TypeError(n + ' is invalid');
+    }
 
+    let x = 0;
 
-
-    if (parseInt(n) != NaN){
-        const x = parseInt(n);
-        // console.log('aaa'); 
+    if (typeof n === 'string') {
+        x = parseInt(n);
+        if ('' + x !== n) {
+            throw new TypeError(n + ' is invalid');
+        }
+    } else if (Number.isInteger(n)){
+        x = n;
     } else {
-        throw new TypeError (n + ' is invalid');
+        throw new TypeError(n + ' is invalid');
     }
 
-    if (n > 0){
-        return result = Array(x).fill(v);
     
-    }else if(n === 0){
-        return [];
-    }else if (n === Infinity || n === -Infinity || n === NaN){
-        throw new Error (n + ' is invalid');
-    }else if (!Number.isInteger(n)){
-        throw new TypeError (n + ' is invalid');
-    }else{
+    if (!Number.isInteger(x)){
+        throw new TypeError(n + ' is invalid');
+    }
+
+    if (x < 0) {
+        throw new TypeError(n + ' is invalid');
+    }
+
+    if (x === 0){
         return [];
     }
+
+    return result = Array(x).fill(v);
  }
 
 // console.log(prefill(3,1), [1,1,1]);
-console.log(prefill(Infinity, undefined), [1,1,1]);
+console.log(prefill(-5, undefined), [1,1,1]);
 
 
 // console.log(prefill(2,'abc'), ['abc','abc']);
