c81077f9653a0530074b23081eab6ff18d396f00
Arvydas Šimbelis (https://github.com/madrakas) <arvydas.simbelis@gmail.com>
2024-03-20 14:08:45 +0200

    cw

---
+++ cw100-150/124.js
@@ -0,0 +1,56 @@
+console.clear();
+
+//reduce to one number
+function reduceByRules(numbers, rules)
+{
+    let newArray = [];
+    let newNumber = numbers[0];
+    let rulesIndex = 0;
+
+    for (let i = 0; i < numbers.length -1; i++) {
+        if (rulesIndex === rules.length) {
+            rulesIndex = 0;
+        }
+        newNumber = rules[rulesIndex](newNumber, numbers[i + 1]);
+        console.log(newNumber);
+        newArray.push(newNumber);
+        rulesIndex++;
+    }
+    return newNumber;
+}
+
+// let rules = [ 
+//     (a, b) => a + b,
+//     (a, b) => a - b 
+// ];
+
+// console.log(reduceByRules([2.0, 2.0, 3.0, 4.0], rules), 5.0);
+
+// rules = [ 
+//     (a, b) => a + b 
+// ];
+
+// console.log(reduceByRules([2.0, 2.0, 2.0], rules), 6.0);
+// console.log(reduceByRules([2.0, 2.0, 2.0, 2.0], rules), 8.0);
+// console.log(reduceByRules([2.0, 2.0, 2.0, 2.0, 2.0], rules), 10.0);
+// console.log(reduceByRules([2.0, 2.0, 2.0, 2.0, 2.0, 2.0], rules), 12.0);
+
+// rules = [
+//     (a, b) => a + b, 
+//     (a, b) => a - b, 
+//     (a, b) => a * b 
+// ];
+
+// console.log(reduceByRules([2.0, 2.0, 2.0], rules), 2.0);
+// console.log(reduceByRules([2.0, 2.0, 2.0, 2.0], rules), 4.0);
+// console.log(reduceByRules([2.0, 2.0, 2.0, 2.0, 2.0], rules), 6.0);
+// console.log(reduceByRules([2.0, 2.0, 2.0, 2.0, 2.0, 2.0], rules), 4.0);
+
+rules = [ 
+    (a, b) => Math.min(a, b),
+    (a, b) => Math.max(a, b) ];
+
+console.log(reduceByRules([1.3, 2.0, 3.3], rules), 3.3);
+console.log(reduceByRules([4.1, 2.2, 2.1, 2.5], rules), 2.2);
+console.log(reduceByRules([8.0, 8.1, 4.1, 12.0, 2.0], rules), 8.0);
+console.log(reduceByRules([2.9, 2.8, 2.7, 2.6, 2.5, 2.4], rules), 2.4);
